f5dfa72c46a6396e549ae1c4ca1ecc08cd193cdc
EiriniVal <eiriniv5@yahoo.gr>
2022-09-09 18:20:05 +0300

    add more functionalities to cs_detector

--- cs_detector.py
+++ cs_detector.py
@@ -8,9 +8,9 @@ import unicodedata
 from my_utils.util import train_furl_model, open_read_lines
 
 
-def get_content(input_file) -> ET.Element:
+def get_root_content(input_file) -> ET.Element:
     """
-    Get content tag of an xml file.
+    Get root and content tag of an xml file.
     :param input_file:
     :return:
     """
@@ -21,7 +21,7 @@ def get_content(input_file) -> ET.Element:
         # get content tag
         content = root.find("content")  # 'xml.etree.ElementTree.Element'
 
-    return content
+    return root, content
 
 
 def get_sentences(content_element: ET.Element) -> ET.Element:
@@ -57,8 +57,10 @@ def generate_sent(sent_xml_elem: ET.Element):
     pass
 
 
-def write_cs_attrib_token():
-    pass
+def write_cs_attrib_token(token_element: ET.Element) -> ET.Element:
+    token_element.set('cs', 'yes')
+    return token_element
+
 
 
 def write_cs_attrib_sent():
@@ -78,19 +80,26 @@ def main():
         # for file in directory (no funct)
         for name in files:
             infile = os.path.join(root, name)
-            # get_content() tag
-            content = get_content(infile)
-
+            tree_root, content = get_root_content(infile)  # get root and content tags
             # generator # for sentence in content (no funct)
             for sentence in get_sentences(content):
 
-                # for token in sentence (no funct)
+                # TOKEN LEVEL CS
                 for token in get_tokens(sentence):
+                    # spaces added around token for better ngram results
+                    token_label = furl_identifier.identify(" "+token.text+" ")  # tuple (label, perplexities dict)
+                    # print(f'{token.text}: {token_label}')
+                    if token_label[0] == "LA":
+                        token = write_cs_attrib_token(token)
+                    # print(type(token))
 
-                    token_label = furl_identifier.identify(" "+token.text+" ")
-                    print(f'{token.text}: {token_label}')
+            # update xml files with info related to code_switching
+            f = open(f'{infile.replace(".xml", "_cs.xml")}', 'wb')
+            # ET.indent(root)
+            f.write(ET.tostring(tree_root, encoding='utf-8', xml_declaration=True))
+            f.close()
 
-    # token_level_cs (apply_lang_model, write_cs_attrib_token)
+
     # save tokens and generate_sent (generate_sent())
     # sentence_level_cs (apply_lang_model, write_cs_attrib_sent())
     # save new xml 
